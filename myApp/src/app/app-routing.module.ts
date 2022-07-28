f067fa731512ef3b89e54c83d5cf4af9f86eaa02
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-28 14:03:38 +0100

    define router for home page and serach page with input var

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {SearchBarComponent} from "./components/search-bar/search-bar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  //home path
+  {
+    path:'',
+    component:HomeComponent
+
+  },
+  //searched  with input variable :
+  {
+    path:'search-page/:game-search',
+    component:SearchBarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
