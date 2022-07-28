395f427927e8307248009b1db2ec6ec972ad3bbc
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-28 14:03:09 +0100

    add Home compo declarations

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -13,11 +13,13 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule } from '@angular/material/input';
 import {MatSelectModule} from "@angular/material/select";
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
