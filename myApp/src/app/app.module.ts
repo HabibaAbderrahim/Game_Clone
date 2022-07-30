76a764e0ad1b2e4801db079ee3543e9a17dbc2d0
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-30 16:54:46 +0100

    add providers

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {GaugeModule} from "angular-gauge";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MatIconModule} from "@angular/material/icon";
@@ -14,6 +14,9 @@ import {MatInputModule } from '@angular/material/input';
 import {MatSelectModule} from "@angular/material/select";
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import {HttpHeaderInterceptor} from "./interceptors/http-headers-interceptor";
+import {HttpErrorInterceptor} from "./interceptors/http-errors-interceptor";
+import {HttpParamsInterceptor} from "./interceptors/http-params-interceptor";
 
 @NgModule({
   declarations: [
@@ -38,7 +41,23 @@ import { HomeComponent } from './components/home/home.component';
 
 
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass: HttpHeaderInterceptor,
+      multi:true
+    },
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass:HttpErrorInterceptor,
+      multi:true
+    },
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass:HttpParamsInterceptor,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
