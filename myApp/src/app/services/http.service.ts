40cd9996c44b872adb0b448c8ead45bb373d89c8
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-31 12:02:06 +0100

    fetGamesDetail

--- myApp/src/app/services/http.service.ts
+++ myApp/src/app/services/http.service.ts
@@ -4,6 +4,7 @@ import {environment as env} from "../../environments/environment";
 import {IGame} from "../models/game";
 import {IAPIResponse} from "../models/apiResponse";
 import {Observable} from "rxjs";
+import {forkJoin, map, pipe} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -18,14 +19,38 @@ export class HttpService {
 
     if(search){
       parms=new HttpParams().set('ordering',ordering).set('search',search);
+
     }
 
-    return this.http.get<IAPIResponse<IGame>>('${env.BASE_URL}/games',{
-      params:parms,
+    return this.http.get<IAPIResponse<IGame>>(`${env.BASE_URL}/games`, {
+      params: parms,
     });
 
 
   }
 
+  getGame(id: string): Observable<IGame> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+  }
 
 }
