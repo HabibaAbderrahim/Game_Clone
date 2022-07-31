5809c4fa3b5829c713d9e74ddf9f1a78ec352c28
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-31 12:03:32 +0100

    NgForm

--- myApp/src/app/components/search-bar/search-bar.component.ts
+++ myApp/src/app/components/search-bar/search-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {HttpService} from "../../services/http.service";
 
 @Component({
   selector: 'app-search-bar',
@@ -8,14 +9,20 @@ import {Router} from "@angular/router";
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+  search :string="";
 
-  constructor(private router : Router) { }
+  constructor(private router : Router ,  private httpService:HttpService ,
+              private activatedRoute:ActivatedRoute
+
+
+  ) { }
 
   ngOnInit(): void {
   }
   onSubmit(f:NgForm){
     //search is the name of input html what's taped by the user see router
-    this.router.navigate(['search-page' , f.value.search])
+    //this.httpService.getGameList('metacrit',this.search=f.value.search)
+    this.router.navigate(['search-page' , this.search])
 
   }
 }
