9bc00d1c468f360184620a72a3583b8f38f1f4f4
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-31 12:02:56 +0100

    navigate to the detail page

--- myApp/src/app/components/home/home.component.ts
+++ myApp/src/app/components/home/home.component.ts
@@ -1,30 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IGame} from "../../models/game";
 import {HttpService} from "../../services/http.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {IAPIResponse} from "../../models/apiResponse";
+
+// @ts-ignore
+import {Subscription} from "rxjs/dist/types";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit , OnDestroy {
 
-  public sort: String | undefined ;
+  public sort: string ="" ;
   public game: Array<IGame> | undefined ;
+  game_search:string="" ;
+  private routeSub : Subscription;
+  private gameSub : Subscription ;
 
 
   constructor(
     private httpService:HttpService ,
+    private router:Router ,
     private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
 
-     this.activatedRoute.params.subscribe((params: Params) => {
+     this.routeSub =this.activatedRoute.params.subscribe((params: Params) => {
        //see path
-      if (params['game-search']) {
-        this.fetchGames('metacrit', params['game-search']);
+      if (params['game_search']) {
+        this.fetchGames('metacrit', this.game_search=params['game_search']);
       } else {
         this.fetchGames('metacrit');
       }
@@ -32,11 +39,26 @@ export class HomeComponent implements OnInit {
   }
 //call method from service and subscribe to the observable to use it data gameList
   fetchGames(sort:string , search?:string):void{
-    this.httpService.getGameList(sort, search).subscribe((gameList:IAPIResponse<IGame>)=>{
+    this.gameSub =this.httpService.getGameList(sort, search).subscribe((gameList:IAPIResponse<IGame>)=>{
       this.game=gameList.results;
       console.log(gameList);
 
     });
+  }
+
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy():void
+  {
+    if(this.gameSub){
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 
 }
